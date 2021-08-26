3b479b07be09f1ed9a2cada6c39f59ef84843b97
soleilyasmina <mounlasolomon@gmail.com>
2021-08-26 14:19:43 -0700

    create function to fetch and append dog images

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // select our dropdown (so we can populate breeds / and add an event listener)
 const dropdown = document.querySelector("#dropdown");
 // select our image container (section) (so we can append / clear out photos)
+const imageContainer = document.querySelector("#image-container");
 
 // 3. CREATE OUR EVENT HANDLERS / FUNCTIONS
 
@@ -11,12 +12,9 @@ const fetchBreeds = async () => {
   try {
     // make our request, and save the response into a variable (called response)
     const response = await axios.get("https://dog.ceo/api/breeds/list/all");
-    // log our data to the console to figure out what to do next
-    console.log(response.data.message);
 
     // use Object.keys on the breed object from our response and save the output in a variable called dogBreeds
     const dogBreeds = Object.keys(response.data.message);
-    console.log(dogBreeds);
 
     // for each of our dog breeds we'd like to...
     dogBreeds.forEach((dogBreed) => {
@@ -32,8 +30,32 @@ const fetchBreeds = async () => {
   }
 }
 fetchBreeds();
+
 // when we select a breed, attach a image of that breed from the API to the DOM
+const fetchAndAttachImage = async (event) => {
+  try {
+    const newDogBreed = event.target.value;
+
+    // make our dog breed specific url
+    const url = `https://dog.ceo/api/breed/${newDogBreed}/images/random`;
+    // make a request to that url and save the output in a variable called response
+    const response = await axios.get(url);
+
+    // clear out our image container
+    imageContainer.innerHTML = "";
+
+    // create an image element
+    const imgEl = document.createElement("img");
+    // set that image element's src to be the message property of our response.data
+    imgEl.src = response.data.message;
+    // append our image element to our imageContainer
+    imageContainer.append(imgEl);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // 4. ADD OUR EVENT LISTENERS
 
-// event listener for our dropdown to get image of breed and attach it
+// event listener for our dropdown to get image of breed and attach it on the change event
+dropdown.addEventListener("change", fetchAndAttachImage);
